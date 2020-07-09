2484fb4c009fc3aeeef11c8c46c16cb991d707cb
jaeyeon <parkjani2@gmail.com>
2020-07-09 09:08:26 +0900

    initial commit

---
+++ chart-on-blog.js
@@ -0,0 +1,71 @@
+function walk(node,func){
+    func(node);
+    node = node.firstChild
+    while(node){
+        walk(node,func);
+        node = node.nextSibling;
+    }
+}
+function parseJSON(strItem){
+    var err;
+    var item = strItem.replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, '"$2": ');
+    try{
+        [err,item] = [null, JSON.parse(item)];
+    }catch(e){
+        return [e,undefined];
+    }
+    if( typeof item === "object" && item !== null){
+        return [null,item];
+    }
+    return [null, undefined];
+}
+function findChartJson(topNode, indicator="%%"){
+    var jsons = []
+    const findNode = (node)=>{
+        let firstChild = node.firstChild
+        if(node && firstChild && node.nodeType == 1 && firstChild.nodeType == 3){
+            if(firstChild.textContent.indexOf(indicator) !== -1){
+                let s_idx = node.textContent.indexOf(indicator);
+                let e_idx = node.textContent.lastIndexOf(indicator);
+                if(s_idx !== -1 && e_idx !== -1){
+                    jsons.push({
+                        node:node,
+                        json:node.textContent.slice(s_idx+indicator.length,e_idx)
+                    })
+                }
+            }
+        }
+    }
+
+    walk(topNode,findNode);
+    console.log(jsons)
+    jsons = jsons.map(item => ({ node:item.node, json:parseJSON(item.json)[1] }))
+                 .filter(item => item.json != undefined)
+                 .filter(item => item.json.type && item.json.data && item.json.data.datasets);
+    console.log(jsons)
+    return jsons;
+}
+
+function drawChart(chartJsons, chartIdPrefix="chart-on-"){
+    const idPrefix = chartIdPrefix;
+
+    return chartJsons.map( (element,idx)=>{
+        let ctx = document.createElement("canvas");
+        ctx.setAttribute("id",idPrefix + idx);
+        element.node.parentNode.replaceChild(ctx, element.node);
+        ctx = document.getElementById(chartIdPrefix+idx);
+        return new Chart(ctx, element.json)
+        })
+}
+
+var previous_onload = window.onload
+window.onload = function(){
+    if(previous_onload){
+        previous_onload();
+    }
+    let body = document.getElementsByTagName("BODY")[0];
+    let chartJsons = findChartJson(body);
+    let result = drawChart(chartJsons)
+}
+
+    
